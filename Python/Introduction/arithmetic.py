25f040d591b494eb527a0b2a8291c7946991d190
nocotan <noconoco.lib@gmail.com>
2016-04-07 20:04:52 +0900

    introduction

---
+++ Python/Introduction/arithmetic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+a = int(input())
+b = int(input())
+
+print(a + b)
+print(a - b)
+print(a * b)
---
+++ Python/Introduction/divmod.py
@@ -0,0 +1,7 @@
+a = int(input())
+b = int(input())
+
+ans = divmod(a, b)
+print(ans[0])
+print(ans[1])
+print(ans)
