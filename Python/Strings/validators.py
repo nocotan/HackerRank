156e210006e7d091ac0a8610905df9bb72e49f82
Masanari KIMURA <noconoco.lib@gmail.com>
2016-04-20 16:01:19 +0000

    validators

---
+++ Python/Strings/validators.py
@@ -0,0 +1,7 @@
+string = str(input())
+
+print(any(c.isalnum() for c in string))
+print(any(c.isalpha() for c in string))
+print(any(c.isdigit() for c in string))
+print(any(c.islower() for c in string))
+print(any(c.isupper() for c in string))
