1d8e8d66d0d48fcc0cf19da0c661e3f8e914cfef
Masanari KIMURA <noconoco.lib@gmail.com>
2016-05-07 09:37:56 +0900

    Delete Day02.py

--- Tutorials/30DaysOfCode/Day02.py
+++
@@ -1,9 +0,0 @@
-cost = float(input())
-tip_per = int(input())
-tax_per = int(input())
-
-tip = cost * tip_per / 100
-tax = cost * tax_per / 100
-
-total = cost + tip + tax
-print("The total meal cost is %d dollars." % round(total))
