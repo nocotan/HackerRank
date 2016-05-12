106fbffde89386b91582af689756f4caa4ab26eb
nocotan <noconoco.lib@gmail.com>
2016-05-12 12:59:39 +0900

    tutor

---
+++ DataStructures/Arrays/arrays.py
@@ -0,0 +1,6 @@
+n = int(input().strip())
+arr = [tmp for tmp in input().strip().split(' ')]
+
+arr.reverse()
+
+print(' '.join(arr))
---
+++ Tutorials/30DaysOfCode/Python/Day07.py
@@ -0,0 +1,4 @@
+n = int(input())
+arr = [tmp for tmp in input().strip().split(' ')]
+arr.reverse()
+print(' '.join(arr))
